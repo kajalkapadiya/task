6d46c442c8974562df6ca3566146f676bff3753b
kajalkapadiya <kajalkapadiya18@gmail.com>
2023-01-01 23:39:44 +0530

    delete from local storage and ul

--- task.js
+++ task.js
@@ -4,25 +4,36 @@ function submit(e) {
     e.preventDefault();
 
     const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const number = document.querySelector('#number').value;
 
     const obj = {
-        email: document.querySelector('#email').value,
-        number: document.querySelector('#number').value
+        name,
+        email,
+        number
     }
 
     const myobj = JSON.stringify(obj);
     localStorage.setItem(name, myobj);
 
     // add also after submit
-    const info = JSON.parse(localStorage.getItem(name))
+    const button = document.createElement('button');
+    button.className = 'btn btn-danger btn-sm float-right delete';
+    button.appendChild(document.createTextNode('delete'));
 
     const x = document.createElement('h4');
     x.className = 'h4';
-    const nameText = document.createTextNode(name);
-    const infoText = document.createTextNode(info);
+    const nameText = document.createTextNode(name + " " + email + " " + number + " ");
 
-    x.appendChild(nameText, infoText);
-
+    x.appendChild(nameText);
+    x.appendChild(button);
     const submit = document.querySelector('.btn');
     submit.after(x);
+
+    button.onclick = function (e) {
+        e.preventDefault();
+
+        localStorage.removeItem(name);
+        document.querySelector('.h4').innerHTML = " ";
+    }
 }
